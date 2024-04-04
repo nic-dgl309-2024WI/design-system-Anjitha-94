963199556242e6e2336f93fc65a2780348d536f1
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2024-04-04 10:29:11 -0700

    Add js code for copy the html code

--- design-system.js
+++ design-system.js
@@ -7,3 +7,18 @@ function myFunction() {
   }
 }
 
+
+
+
+function copyCode() {
+  const textarea = document.createElement('textarea');
+  const code = document.querySelector('pre code').innerText;
+  textarea.value = code;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  // Optional: Display a confirmation
+  alert('Code copied to clipboard!');
+}
+
